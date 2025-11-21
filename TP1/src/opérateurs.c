e6d6246787d76af1d23cc84a9250616cc632e977
Anthony-Faria-dos-santos <144596792+Anthony-Faria-dos-santos@users.noreply.github.com>
2025-11-21 14:35:02 +0100

    feat: TP1_ex1.5 Add C source files for variables and operators.

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+
+    // 1. Addition
+    printf("a + b = %d\n", a + b);
+
+    // 2. Soustraction
+    printf("a - b = %d\n", a - b);
+
+    // 3. Multiplication
+    printf("a * b = %d\n", a * b);
+
+    // 4. Division
+    printf("a / b = %d\n", a / b);
+
+    // 5. Modulo
+    printf("a %% b = %d\n", a % b);
+
+    // 6. Comparaison d'égalité
+    printf("a == b : %d\n", a == b);
+
+    // 7. Comparaison de supériorité
+    printf("a > b : %d\n", a > b);
+
+    return 0;
+}
